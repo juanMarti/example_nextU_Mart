813d2c977ab3681e62a9ad71b2c6276d2b1f065c
MARTIN CHAVEZ ANTO <martinchavez@macbook-pro-de-martin.local>
2016-07-30 21:35:30 -0500

    comentar Mart

--- EjercicoDeCreacion/ViewController.swift
+++ EjercicoDeCreacion/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendName(sender: AnyObject) {
+        
+        messageLabel.text = "Martín"
+    }
 
+    @IBAction func sendSurname(sender: AnyObject) {
+        
+           messageLabel.text = "Chávez"
+    }
+    
+    @IBAction func sendCity(sender: AnyObject) {
+        
+           messageLabel.text = "Piura"
+    }
+    
+    @IBAction func sendCountry(sender: AnyObject) {
+        
+           messageLabel.text = "Perú"
+    }
+    
+    @IBAction func senderButton(sender: AnyObject) {
+        
+         messageLabel.text = "Martín"
+    }
 }
 
+//Comentary Mart
